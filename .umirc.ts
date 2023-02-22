cfcc4c686e6fa51f3c33ebe375a9471f7183e0f8
chenping3 <chenping3@finchina.com>
2023-02-22 17:25:29 +0800

    测试

--- .umirc.ts
+++ .umirc.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from "umi";
-
-export default defineConfig({
+export default {
   npmClient: "pnpm",
+  // 启动api路由
   apiRoute: {
     platform: "vercel",
   },
   routes: [
-    { exact: true, path: "/", component: "index" },
-    { exact: true, path: "/posts/create", component: "posts/create" },
-    { exact: true, path: "/login", component: "login" },
-    { exact: true, path: "/posts/:postId", component: "posts/post" },
+    { path: "/", component: "index" },
+    { path: "/posts/create", component: "posts/create" },
+    { path: "/login", component: "login" },
+    { path: "/posts/:postId", component: "posts/post" },
   ],
-  plugins: [require.resolve("@umijs/plugins/dist/tailwindcss")],
   tailwindcss: {},
-});
+  plugins: ["@umijs/plugins/dist/tailwindcss"],
+};
